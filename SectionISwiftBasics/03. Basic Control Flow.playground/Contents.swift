d11eb0ab9f75c046081b11ab5df69bd586026273
Ramon Audain <raudain123@gmail.com>
2020-10-18 15:35:16 -0700

    Working on chapter 3

---
+++ SectionISwiftBasics/03. Basic Control Flow.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+// Challenge 2: Boolean challenge
+let answer = true && true
+let answer1 = false || false
+let answer2 = (true && 1 != 2) || (4 > 3 && 100 < 1)
+let answer3 = ((10 / 2) > 3) && ((10 % 2) == 0)
+
+
+// Challenge 3: Snakes and ladders
+let currentPosition = 1
+let diceRoll = Int.random(in: 1...6)
+
+if 
